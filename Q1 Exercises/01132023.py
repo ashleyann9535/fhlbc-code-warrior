05b79f8e91530ed4e801d24835c511f5f0e38627
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-01-13 10:59:40 -0600

    complete bank robbery exercise

--- Q1 Exercises/01132023.py
+++ Q1 Exercises/01132023.py
@@ -1,2 +1,11 @@
-#First Python Exercise. Use print() to tell us your name.
+#Python Exercise 2. Use print() to tell us your name.
 print("Shlee")
+
+# Bank Robbery Money
+# I just robbed a bank with some of my friends.  We got away with $19,867,324,678,987.99, 
+# and now we have to split it up 5 ways.  Using the cash  variable I've already defined for you, 
+# print out the dollar amount each robber gets to keep.  (divide cash  by 5 and print the answer out)
+# p.s. I haven't actually robbed a bank.  Yet.
+
+cash = 19867324678987.99/5
+print("{:.2f}".format(cash))

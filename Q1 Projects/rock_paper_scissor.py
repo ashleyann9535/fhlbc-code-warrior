7c7816c1334ab8e2a2cce1075cadb578d51b20fa
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-02-16 19:16:35 -0600

    change extra if statements to elif

--- Q1 Projects/rock_paper_scissor.py
+++ Q1 Projects/rock_paper_scissor.py
@@ -10,17 +10,17 @@ print("SHOOT!")
 if player1 =='rock':
     if player2 == 'paper':
         print('player2 win!')
-    if player2 == 'scissors':
+    elif player2 == 'scissors':
         print('player1 wins')
 elif player1 == 'scissors':
     if player2 == 'paper':
         print('player1 wins')
-    if player2 == 'rock':
+    elif player2 == 'rock':
         print('player2 win!')
 elif player1 == 'paper':
     if player2 == 'rock':
         print('player1 wins')
-    if player2 == 'scissors':
+    elif player2 == 'scissors':
         print('player2 win!')
 elif player1 == player2:
     print('tie game')

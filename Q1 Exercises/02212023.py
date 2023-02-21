ec0071e5ded12d762d84e290a9e8b9e572845daf
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-02-21 16:38:08 -0600

    complete exercise 13 loop and range

---
+++ Q1 Exercises/02212023.py
@@ -0,0 +1,9 @@
+#Exercise 13 - For Loop and Range Exercise
+#Use a for loop to add up every odd number from 10 to 20 (inclusive) 
+#and store the result in the variable x .
+
+sum = 0
+
+for x in range(11, 21, 2):
+    sum += x 
+print(sum)

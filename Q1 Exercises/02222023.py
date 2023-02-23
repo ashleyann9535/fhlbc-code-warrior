de356bfd3c18b6d82a35d8d23a24c3f556815b47
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-02-22 20:15:39 -0600

    complete exercise 14 while loop

--- Q1 Exercises/02222023.py
+++ Q1 Exercises/02222023.py
@@ -7,8 +7,22 @@
 
 #Extra Practice - Stop Copying Me
 #Play the copy game until inputs "stop copying me"
-start = input('Hey there! How is it going? ')
-stop = 'stop copying me'
+# start = input('Hey there! How is it going? ')
+# stop = 'stop copying me'
 
-while start != stop:
-    start = input(f'{start} \n').lower()
+# while start != stop:
+#     start = input(f'{start} \n').lower()
+
+#Exercise 14 - While Loop Exercise
+# Use a while loop to generate a random number between 1 and 10 until you get the number 5. 
+# Every time the loop runs, increment the variable i .
+from random import randint  # use randint(a, b) to generate a random number between a and b
+
+number = 0 # store random number in here, each time through
+i = 0  # i should be incremented by one each iteration
+
+while number != 5:
+    i += 1
+    number = randint(1, 10)
+    print(number)
+print(i)

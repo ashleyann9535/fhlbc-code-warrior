dcd4c03a60122241147592a9f8a6413037eb98e7
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-03-22 18:42:52 -0500

    complete index of second occurance challenge

--- Code_Wars_python/03222023.py
+++ Code_Wars_python/03222023.py
@@ -11,4 +11,27 @@ def div_con(x):
             total -= int(i)
     return total
 
-print(div_con([9, 3, '7', '3'])) #2
+#print(div_con([9, 3, '7', '3'])) #2
+
+# Find the index of the second occurrence of a letter in a string
+# In this kata, you need to write a function that takes a string and a letter as input and 
+# then returns the index of the second occurrence of that letter in the string. 
+# If there is no such letter in the string, then the function should return -1. 
+# If the letter occurs only once in the string, then -1 should also be returned.
+
+def second_symbol(s, symbol):
+    round = 0
+    count = 0
+    for char in s:
+        round += 1
+        if char == symbol:
+            count +=1
+        if count == 2:
+            break
+    if count == 2:
+        return round - 1
+    else:
+        return -1
+
+print(second_symbol('Hello world!!!','l')) # 3
+print(second_symbol('Hello world!!!', 'A')) # -1

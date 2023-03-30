e7f55365ddafe42a4e3833b54526c7624519c9c9
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-03-30 17:17:10 -0500

    complete Exercise 21: More List Comprehension

--- Q1_Exercises/03302023.py
+++ Q1_Exercises/03302023.py
@@ -8,9 +8,27 @@
 
 people = ["Elie", "Tim", "Matt"]
 answer = [person[0].upper() for person in people]
-print(answer)
+#print(answer)
 
 numbers = [1,2,3,4,5,6]
 answer2 = [num for num in numbers if num%2 == 0]
-print(answer2)
+#print(answer2)
 
+#Exercise 21 More List Comprehension Exercises
+#1. Given two lists [1,2,3,4] and [3,4,5,6], create a variable called answer, which is a new list 
+# that is the intersection of the two. Your output should be [3,4] .  
+# Hint: use the in  operator to test whether an element is in a list.  
+# For example:  5 in [1,5,2]  is True.  3 in [1,5,2]  is False.
+
+#2. Given a list of words ["Elie", "Tim", "Matt"] create a variable called answer2, 
+# which is a new list with each word reversed and in lower case (use a slice to do the reversal!) 
+# Your output should be ['eile', 'mit', 'ttam'] 
+
+list1 = [1,2,3,4]
+list2 = [3,4,5,6]
+answer3 = [x for x in list1 if x in list2]
+print(answer3)
+
+names = ["Elie", "Tim", "Matt"]
+answer4 = [name.lower() [::-1] for name in names]
+print(answer4)

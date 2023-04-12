69111650b1412092e2d9c988db6289a63fb74a22
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-04-11 20:14:09 -0500

    complete Exercise 25 One More Nested List Comp Challenge

--- Q1_Exercises/04112023.py
+++ Q1_Exercises/04112023.py
@@ -3,3 +3,8 @@
 #with the following value - [[0, 1, 2], [0, 1, 2], [0, 1, 2]] . 
 nested_list = [[x for x in range(0,3)] for l in range(0,3)]
 print(nested_list)
+
+#Exercise 25 One More Nested List Comp Challenge
+#Using list comprehension make a 10x10 nested list.  10 rows, each row contains the numbers 0-9.
+matrix = [[x for x in range(0,10)] for i in range(0,10)]
+print(matrix)

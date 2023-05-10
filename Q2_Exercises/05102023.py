55e5bff0c430f6e3508bb643c2d4cbba9e1508dc
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-05-10 18:38:27 -0500

    complete exercise 30: .fromkeys

--- Q2_Exercises/05102023.py
+++ Q2_Exercises/05102023.py
@@ -20,3 +20,11 @@ if food in bakery_stock:
     print(f"{bakery_stock[food]} left")
 else:
     print("We don't make that")
+
+#Exercise 30 - Fromkeys Exercise
+#DO NOT TOUCH game_properties!
+game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo", "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notifications", "achievements"] 
+# Use the game_properties list and dict.fromkeys() to generate a dictionary 
+# with all values set to 0. Save the result to a variable called initial_game_state
+initial_game_state= {}.fromkeys(game_properties, 0)
+print(initial_game_state)

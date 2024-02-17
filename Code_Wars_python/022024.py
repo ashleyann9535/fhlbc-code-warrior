549e86bf13c43bf7af5c5917b5c1b35162e16992
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2024-02-17 09:18:16 -0600

    complete algo

--- Code_Wars_python/022024.py
+++ Code_Wars_python/022024.py
@@ -1,3 +1,5 @@
+#Source = www.codewars.com
+
 #Friend or Foe
 def friend(x):
     friends = filter(lambda name: len(name) == 4, x )
@@ -8,5 +10,17 @@ def friend_two(x):
     return friends_two
 
 
-print(friend(["Ryan", "Kieran", "Mark"])) #["Ryan", "Mark"]
-print(friend_two(["Ryan", "Kieran", "Mark"])) #["Ryan", "Mark"]
+#print(friend(["Ryan", "Kieran", "Mark"])) #["Ryan", "Mark"]
+#print(friend_two(["Ryan", "Kieran", "Mark"])) #["Ryan", "Mark"]
+
+#I love you, a little , a lot, passionately ... not at all
+def how_much_i_love_you(num):
+    phrases = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+
+    location = (num % 6) -1
+
+    return phrases[location]
+
+print(how_much_i_love_you(2)) # 'a little
+print(how_much_i_love_you(6)) # 'not at all
+print(how_much_i_love_you(11))# madly 

a9aaeb7abf86a857b2dd87934c9b9cff2b00c3ee
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2024-04-11 21:24:31 -0500

    solve green valley

--- Code_Wars_python/042024.py
+++ Code_Wars_python/042024.py
@@ -8,6 +8,33 @@ def step_through_with(s):
 
     return False
 
-print(step_through_with('moon')) #True
-print(step_through_with('sun')) #False
-print(step_through_with('test')) #False
+# print(step_through_with('moon')) #True
+# print(step_through_with('sun')) #False
+# print(step_through_with('test')) #False
+
+#How Green Is My Valley?
+def make_valley(arr):
+    if len(arr) < 2:
+        return arr
+    
+    right = []
+    left = []
+    index = 1
+
+    arr_sort = sorted(arr, reverse=True)
+
+    for num in arr_sort[:-1]:
+        if index % 2 != 0:
+            left.append(num)
+        else:
+            right.insert(0,num)
+
+        index += 1
+
+    left.append(arr_sort[-1])
+    left.extend(right)
+
+    return left
+
+print(make_valley([79, 35, 54, 19, 35, 25])) #[79, 35, 25, *19*, 35, 54]
+print(make_valley([67, 93, 100, -16, 65, 97, 92])) #[100, 93, 67, *-16*, 65, 92, 97]
